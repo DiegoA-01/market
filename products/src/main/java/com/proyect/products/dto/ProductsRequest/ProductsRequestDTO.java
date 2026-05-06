2624a91db9285bf30b4315b4cd7db113437e0ca1
DiegoA-01 <pinedamontenegrodiegoalejandro@gmail.com>
2026-05-05 21:18:43 -0500

    Actualiza

--- products/src/main/java/com/proyect/products/dto/ProductsRequest/ProductsRequestDTO.java
+++ products/src/main/java/com/proyect/products/dto/ProductsRequest/ProductsRequestDTO.java
@@ -1,6 +1,5 @@
 package com.proyect.products.dto.ProductsRequest;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
