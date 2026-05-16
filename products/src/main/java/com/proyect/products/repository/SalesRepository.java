bbd878269981afcc467f35c70f0bc3c7dcc7b1d3
NavarroSantiago-0902 <santiago.navarrov@uqvirtual.edu.co>
2026-05-15 19:49:25 -0500

    Feature: Se actualiza repository

--- products/src/main/java/com/proyect/products/repository/SalesRepository.java
+++ products/src/main/java/com/proyect/products/repository/SalesRepository.java
@@ -1,7 +1,5 @@
 package com.proyect.products.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,4 @@ import com.proyect.products.entity.Sales;
 
 @Repository
 public interface SalesRepository extends JpaRepository<Sales, Long>{
-    List<Sales> findByUser_IdUser(Long userId);
 }
